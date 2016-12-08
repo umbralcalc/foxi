4103e265b1a12dc222cd15e81f3f45774a917f7f
umbralcalc <umbralcalc@gmail.com>
2016-12-08 00:28:37 +0000

    Changes

--- foxiscripts/sample_script.py
+++ foxiscripts/sample_script.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.insert(0, 'INSERT YOUR PATH TO FOXI HERE' + '/foxi/foxisource/foxi.py')
+sys.path.insert(0, '' + '/foxi/foxisource/foxi.py')
 from foxi import foxi
 
 model_list = ['hi','mhi'] # List the model names to compute utilities
 chains_column_numbers = [1,2] # Which columns are the relevant parameters in the chains? Order matters 
 prior_column_numbers = [1,2] # Which columns are the relevant parameters in the prior? Order matters 
-number_of_points = 100 # How many points to read in from the forecast data chains
-number_of_prior_points = 100 # How many points to read in from the prior samples
+number_of_points = 10 # How many points to read in from the forecast data chains
+number_of_prior_points = 10 # How many points to read in from the prior samples
 error_vector = [0.1,0.1] # Set some future predictions for the measurements on each parameter corresponding to the columns in e.g. 'chains_column_numbers'
 
 new_foxi_instance = foxi('INSERT YOUR PATH TO FOXI HERE') # Fire up a new instance of foxi
